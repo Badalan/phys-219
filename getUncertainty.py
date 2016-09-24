7722d29a44256d16fe2cb44918558622d50723f6
VanderpoelLiam <vanderpoel.liam@gmail.com>
2016-09-24 15:57:13 -0700

    Change variance.py to getUncertainty.py

--- getUncertainty.py
+++ getUncertainty.py
@@ -1,12 +1,14 @@
 # !python3
 import numpy as np
 
+
 def variance(N, data, avg):
     sums = 0
     for x in data:
         avgDiff = x - avg
         sums += avgDiff**2
     return (1/(N-1))*sums
+
 
 def stdev(vary):
     return np.sqrt(vary)
