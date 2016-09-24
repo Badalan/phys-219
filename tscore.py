165e36f02e2a83c637622e521589fec992435855
VanderpoelLiam <vanderpoel.liam@gmail.com>
2016-09-24 15:42:15 -0700

    Tscore require cmd line args

--- tscore.py
+++ tscore.py
@@ -2,13 +2,13 @@
 import sys
 import math
 
-# A = float(sys.argv[1])
-# B = float(sys.argv[2])
-# dA = float(sys.argv[3])
-# dB = float(sys.argv[4])
+A = float(sys.argv[1])
+B = float(sys.argv[2])
+dA = float(sys.argv[3])
+dB = float(sys.argv[4])
 
 
 def tscore(A, B, dA, dB):
     return (math.fabs(A - B)/math.sqrt(dA**2 + dB**2))
 
-# print(tscore(A, B, dA, dB))
+print(tscore(A, B, dA, dB))
