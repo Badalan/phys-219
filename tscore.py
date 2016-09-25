3a0b644da9b5b237f68756dbdab31d9d947a5007
Alex Badalan <alexbadalan@dhcp-128-189-91-139.ubcsecure.wireless.ubc.ca>
2016-09-24 20:17:02 -0700

    liam is gay

--- tscore.py
+++ tscore.py
@@ -6,9 +6,10 @@ A = float(sys.argv[1])
 B = float(sys.argv[2])
 dA = float(sys.argv[3])
 dB = float(sys.argv[4])
+l
 
 
 def tscore(A, B, dA, dB):
     return (math.fabs(A - B)/math.sqrt(dA**2 + dB**2))
 
-print(tscore(A, B, dA, dB))
+print("Liam is gay")
