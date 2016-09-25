c4caeb960e33a8c03ba506f84f7117cf33d2f253
Alex Badalan <alexbadalan@dhcp-128-189-91-139.ubcsecure.wireless.ubc.ca>
2016-09-24 20:25:00 -0700

    bonsoir

--- tscore.py
+++ tscore.py
@@ -6,8 +6,6 @@ A = float(sys.argv[1])
 B = float(sys.argv[2])
 dA = float(sys.argv[3])
 dB = float(sys.argv[4])
-l
-
 
 def tscore(A, B, dA, dB):
     return (math.fabs(A - B)/math.sqrt(dA**2 + dB**2))
